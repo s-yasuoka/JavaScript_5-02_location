ce593a8fce33d1d9381b7357dc87861a1462db96
s-yasuoka <migayasu@gmail.com>
2022-06-17 15:58:11 +0900

    [add]selector

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 'use strict'
 
-document.getElementById('form').ariaSelect.onchange = function(){
-  location.href = document.getElementById('form').ariaSelected.valueOf;
+const lang= document.querySelector('html').lang;
+
+if (lang == 'ja'){
+  document.querySelector('option[value="index.html"]').selected = true;
+} else if (lang == 'en'){
+  document.querySelector('option[value="index-en.html"]').selected = true;
+} else if (lang =='zh'){
+  document.querySelector('option[value="index-zh.html"]').selected = true;
+}
+
+
+document.getElementById('form').select.onchange = function(){
+  location.href = document.getElementById('form').select.value;
 }
